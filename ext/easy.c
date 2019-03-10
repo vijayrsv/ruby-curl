eb9efe40cdf8e29d72b87d78abd9d3f3041807ad
vijayrsv <vijayrepomailzbox@gmail.com>
2019-03-10 18:30:04 +0530

    Curl set_opt API is added.

--- ext/easy.c
+++ ext/easy.c
@@ -39,9 +39,31 @@ static VALUE ruby_curl_easy_initialize(int argc, VALUE *argv, VALUE self) {
 
 
 	curl_easy_setopt(rb_curl->cp, CURLOPT_URL, RSTRING_PTR(url));
-	//curl_easy_setopt(rb_curl->cp, CURLOPT_RETURNTRANSFER, 1);
 
   return self;
+}
+
+static VALUE ruby_curl_easy_set_opt(VALUE self, VALUE opt, VALUE val) {
+	ruby_curl *rb_curl;
+	long option = NUM2LONG(opt);
+
+	Data_Get_Struct(self, ruby_curl, rb_curl);
+
+	switch (option) {
+		case CURLOPT_URL:
+			curl_easy_setopt(rb_curl->cp, CURLOPT_URL, RSTRING_PTR(val));
+			break;
+		case CURLOPT_POST:
+			curl_easy_setopt(rb_curl->cp, CURLOPT_POST, NUM2LONG(val));
+			break;
+		case CURLOPT_TIMEOUT:
+			curl_easy_setopt(rb_curl->cp, CURLOPT_TIMEOUT, NUM2LONG(val));
+			break;
+		default:
+			rb_raise(rb_eTypeError, "Unsupported option.");
+	}
+
+	return Qnil;
 }
 
 static VALUE ruby_curl_easy_exec(VALUE self) {
@@ -62,6 +84,7 @@ void init_easy() {
 	cCurlEasy = rb_define_class_under(mCurl, "Easy", rb_cObject);
 	rb_define_alloc_func(cCurlEasy, ruby_curl_allocate);
   rb_define_method(cCurlEasy, "initialize", ruby_curl_easy_initialize, -1);
+  rb_define_method(cCurlEasy, "set_opt", ruby_curl_easy_set_opt, 2);
   rb_define_method(cCurlEasy, "exec", ruby_curl_easy_exec, 0);
   rb_define_method(cCurlEasy, "close", ruby_curl_easy_close, 0);
 }
